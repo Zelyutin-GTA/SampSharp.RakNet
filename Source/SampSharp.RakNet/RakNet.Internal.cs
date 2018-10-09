27b6f996a4c438b771713c25518694df8424f538
Zelyutin <danil.zelyutin@gmail.com>
2018-10-09 08:37:40 +0500

    Added drafts for most of natives except BS_[Write/Read]Value, added enums Some natives will be done later adding reference types for some of arguments BS_WriteValue and BS_ReadValue will be implemented separatly because they should be built with NativeLoader cause of their floating params numbers (At this momenth this is the only way that I know).

--- Source/SampSharp.RakNet/RakNet.Internal.cs
+++ Source/SampSharp.RakNet/RakNet.Internal.cs
@@ -12,6 +12,58 @@ namespace SampSharp.RakNet
     }
     public partial class RakNet
     {
+        public enum PR_HandlerType
+        {
+            PR_INCOMING_RPC,
+            PR_INCOMING_PACKET,
+            PR_OUTCOMING_RPC,
+            PR_OUTCOMING_PACKET,
+            PR_NUMBER_OF_HANDLER_TYPES
+        };
+
+        public enum PR_ValueType
+        {
+            PR_INT8,
+            PR_INT16,
+            PR_INT32,
+            PR_UINT8,
+            PR_UINT16,
+            PR_UINT32,
+            PR_FLOAT,
+            PR_BOOL,
+            PR_STRING,
+
+            // compressed
+            PR_CINT8,
+            PR_CINT16,
+            PR_CINT32,
+            PR_CUINT8,
+            PR_CUINT16,
+            PR_CUINT32,
+            PR_CFLOAT,
+            PR_CBOOL,
+            PR_CSTRING,
+
+            PR_BITS
+        };
+
+        public enum PR_PacketPriority
+        {
+            PR_SYSTEM_PRIORITY,
+            PR_HIGH_PRIORITY,
+            PR_MEDIUM_PRIORITY,
+            PR_LOW_PRIORITY
+        };
+
+        public enum PR_PacketReliability
+        {
+            PR_UNRELIABLE = 6,
+            PR_UNRELIABLE_SEQUENCED,
+            PR_RELIABLE,
+            PR_RELIABLE_ORDERED,
+            PR_RELIABLE_SEQUENCED
+        };
+
         protected static RakNetInternal Internal;
 
         static RakNet()
@@ -20,24 +72,117 @@ namespace SampSharp.RakNet
         }
         public class RakNetInternal
         {
-            //Native example from streamer:
-            /*
-            [NativeMethod(Function = "Streamer_SetPlayerTickRate")]
-            public virtual int SetPlayerTickRate(int playerid, int rate)
-            {
-                throw new NativeNotImplementedException();
-            }
-            */
-            [NativeMethod(Function = "CallRemoteFunction")]
-            public virtual int CallRemoteFunction(string functionName, string argumentsFormat)
-            {
-                throw new NativeNotImplementedException();
-            }
-            [NativeMethod(Function = "BS_New")]
+            #region Pawn.RakNet natives
+
+            #region Main
+            [NativeMethod]
             public virtual int BS_New()
             {
                 throw new NativeNotImplementedException();
             }
+
+            [NativeMethod]
+            public virtual int BS_Delete(int bs) // TODO: bs is ref-type
+            {
+                throw new NativeNotImplementedException();
+            }
+
+            [NativeMethod]
+            public virtual int BS_RPC(int bs, int playerid, int rpcid, int priority = (int)PR_PacketPriority.PR_HIGH_PRIORITY, int reliability = (int)PR_PacketReliability.PR_RELIABLE_ORDERED) // priority and reliability -> enums
+            {
+                throw new NativeNotImplementedException();
+            }
+
+            [NativeMethod]
+            public virtual int BS_Send(int bs, int playerid, int priority = (int)PR_PacketPriority.PR_HIGH_PRIORITY, int reliability = (int)PR_PacketReliability.PR_RELIABLE_ORDERED) // priority and reliability -> enums
+            {
+                throw new NativeNotImplementedException();
+            }
+            #endregion
+
+
+            #region Resets
+            [NativeMethod]
+            public virtual int BS_Reset(int bs)
+            {
+                throw new NativeNotImplementedException();
+            }
+
+            [NativeMethod]
+            public virtual int BS_ResetReadPointer(int bs)
+            {
+                throw new NativeNotImplementedException();
+            }
+
+            [NativeMethod]
+            public virtual int BS_ResetWritePointer(int bs)
+            {
+                throw new NativeNotImplementedException();
+            }
+
+            [NativeMethod]
+            public virtual int BS_IgnoreBits(int bs, int number_of_bits)
+            {
+                throw new NativeNotImplementedException();
+            }
+            #endregion
+
+            #region Offsets
+            [NativeMethod]
+            public virtual int BS_SetWriteOffset(int bs, int offset)
+            {
+                throw new NativeNotImplementedException();
+            }
+            [NativeMethod]
+            public virtual int BS_GetWriteOffset(int bs, int offset) // TODO: offset ref-type
+            {
+                throw new NativeNotImplementedException();
+            }
+            [NativeMethod]
+            public virtual int BS_SetReadOffset(int bs, int offset)
+            {
+                throw new NativeNotImplementedException();
+            }
+            [NativeMethod]
+            public virtual int BS_GetReadOffset(int bs, int offset) // TODO: offset ref-type
+            {
+                throw new NativeNotImplementedException();
+            }
+            #endregion
+
+            #region
+            [NativeMethod]
+            public virtual int BS_GetNumberOfBitsUsed(int bs, int number) // TODO: number ref-type
+            {
+                throw new NativeNotImplementedException();
+            }
+            [NativeMethod]
+            public virtual int BS_GetNumberOfBytesUsed(int bs, int number) // TODO: number ref-type
+            {
+                throw new NativeNotImplementedException();
+            }
+
+            [NativeMethod]
+            public virtual int BS_GetNumberOfUnreadBits(int bs, int number) // TODO: number ref-type
+            {
+                throw new NativeNotImplementedException();
+            }
+
+            [NativeMethod]
+            public virtual int BS_GetNumberOfBitsAllocated(int bs, int number) // TODO: number ref-type
+            {
+                throw new NativeNotImplementedException();
+            }
+            #endregion
+
+            #endregion
+            #region Common Utils
+            [NativeMethod]
+            public virtual int CallRemoteFunction(string functionName, string argumentsFormat)
+            {
+                throw new NativeNotImplementedException();
+            }
+            #endregion
 
 
             // Raknet Natives:
