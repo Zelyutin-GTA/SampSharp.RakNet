33903e05b12b4889e8dff0f9a5361154febe6231
Danil Zelyutin <danil.zelyutin@gmail.com>
2018-10-17 15:08:44 +0500

    Added Passenger sync writing

--- Source/SampSharp.RakNet/Syncs/PassengerSync.cs
+++ Source/SampSharp.RakNet/Syncs/PassengerSync.cs
@@ -40,6 +40,14 @@ namespace SampSharp.RakNet.Syncs
         {
             this.Read(true);
         }
+        public void WriteIncoming()
+        {
+            this.Write(false);
+        }
+        public void WriteOutcoming()
+        {
+            this.Write(true);
+        }
         private void Read(bool outcoming)
         {
             //ReadSync() playerID;
@@ -93,7 +101,6 @@ namespace SampSharp.RakNet.Syncs
                 ParamType.UINT16, "lrKey",
                 ParamType.UINT16, "udKey",
                 ParamType.UINT16, "keys"
-
             };
             if (outcoming)
             {
@@ -103,6 +110,40 @@ namespace SampSharp.RakNet.Syncs
 
             BS.ReadValue(arguments.ToArray());
             //Need to divide up the reading cause of native arguments limit(32) in SampSharp.
+        }
+        private void Write(bool outcoming)
+        {
+            var arguments = new List<object>()
+            {
+                ParamType.UINT8, this.packetID,
+                ParamType.UINT16, this.vehicleID,
+                ParamType.BITS, this.driveBy, 2,
+                ParamType.BITS, this.seatID, 6,
+                ParamType.BITS, this.additionalKey, 2,
+                ParamType.BITS, this.weaponID, 6,
+                ParamType.UINT8, this.playerHealth,
+                ParamType.UINT8, this.playerArmour,
+                ParamType.UINT16, this.lrKey,
+                ParamType.UINT16, this.udKey,
+                ParamType.UINT16, this.keys,
+            };
+
+            if (outcoming)
+            {
+                arguments.Insert(2, ParamType.UINT16);
+                arguments.Insert(3, this.fromPlayerID);
+            }
+
+            BS.WriteValue(arguments.ToArray());
+
+            arguments = new List<object>()
+            {
+                ParamType.FLOAT, this.position.X,
+                ParamType.FLOAT, this.position.Y,
+                ParamType.FLOAT, this.position.Z,
+            };
+
+            BS.WriteValue(arguments.ToArray());
         }
     }
 }
