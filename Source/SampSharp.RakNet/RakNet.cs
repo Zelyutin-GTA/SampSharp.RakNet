a745938767d69242dc074acf7a617ff1c08c8d8b
Danil Zelyutin <danil.zelyutin@gmail.com>
2018-10-18 12:58:58 +0500

    Made logging params of RakNet public

--- Source/SampSharp.RakNet/RakNet.cs
+++ Source/SampSharp.RakNet/RakNet.cs
@@ -45,12 +45,12 @@ namespace SampSharp.RakNet
 
         #endregion
 
-        bool LoggingIncomingRPC { get; set; } = false;
-        bool LoggingOutcomingRPC { get; set; } = false;
-        bool LoggingIncomingPacket { get; set; } = false;
-        bool LoggingOutcomingPacket { get; set; } = false;
-        bool LoggingBlockingRPC { get; set; } = false;
-        bool LoggingBlockingPacket { get; set; } = false;
+        public bool LoggingIncomingRPC { get; set; } = false;
+        public bool LoggingOutcomingRPC { get; set; } = false;
+        public bool LoggingIncomingPacket { get; set; } = false;
+        public bool LoggingOutcomingPacket { get; set; } = false;
+        public bool LoggingBlockingRPC { get; set; } = false;
+        public bool LoggingBlockingPacket { get; set; } = false;
 
         public void SetLogging(bool incomingRPC, bool outcomingRPC, bool incomingPacket, bool outcomingPacket, bool blockingRPC, bool blockingPacket)
         {
